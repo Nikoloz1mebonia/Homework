a899bb48f95a6c27187bcc6c52f26b746a190e29
Nikoloz1mebonia <127876601+Nikoloz1mebonia@users.noreply.github.com>
2023-04-05 20:56:13 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,69 +1,56 @@
-# დავალება 1
-class Rectangle:
-
-    def __init__(self, width, length, color):
-        self.width = width
-        self.length = length
-        self.color = color
-
-    def perimeter(self):
-        return 2 * (self.width + self.length)
-
-    def area(self):
-        return self.width * self.length
-
-
-obj_1 = Rectangle(1, 5, "blue")
-obj_2 = Rectangle(3, 3, "green")
-obj_3 = Rectangle(3, 7, "purple")
-print(obj_1.area())
-
-
-# დავალება 2
-class Car:
-
-    def __init__(self, color, brand, model):
-        self.color = color
-        self.brand = brand
-        self.model = model
+class Person:
+    def __init__(self, name, gender, age, income):
+        self.name = name
+        self.gender = gender.lower()
+        self.age = age
+        self.income = income
 
     def __str__(self):
-        return f"მანქანის ბრენდი: {self.brand}, მანქანის მოდელი: {self.model}, მანქანის ფერი: {self.color}"
+        return f"name: {self.name}, gender: {self.gender}, age: {self.age}, income: {self.income}"
+
+    def expense(self):
+        x = (self.income * 20) / 100
+        y = (self.income * 1) / 100
+        return x + y
+
+    def danazogi(self):
+        if self.age < 30:
+            if self.gender == "female":
+                return f"danazogi: {30 * (self.income * 1) / 100}"
+            elif self.gender == "male":
+                return f"danazogi: {35 * (self.income * 1) / 100}"
+            else:
+                return "error"
+        elif self.age == 30:
+            if self.gender == "female":
+                return f"danazogi: {30 * (self.income * 1) / 100}"
+            elif self.gender == "male":
+                return f"danazogi: {35 * (self.income * 1) / 100}"
+            else:
+                return "error"
+        elif self.age > 30:
+            if self.gender == "female":
+                x = 60 - self.age
+                return f"danazogi: {x * (self.income * 1) / 100}"
+            elif self.gender == "male":
+                x = 65 - self.age
+                return f"danazogi: {x * (self.income * 1) / 100}"
+            else:
+                return "error"
+        else:
+            return "error"
+
+    def year_amount(self):
+        if self.gender == "male":
+            gap = 65 - self.age
+            return f"you have got {gap} years until pension"
+        elif self.gender == "female":
+            gap1 = 60 - self.age
+            return f"you have got {gap1} years until pension"
 
 
-car_1 = Car("red", "ford", "mustang")
-car_2 = Car("blue", "toyota", "prius")
-car_3 = Car("green", "volkswagen", " golf")
-print(car_1)
-print(car_2)
-print(car_3)
-
-
-# დავალება 3
-class Dog:
-    def __init__(self, breed, size, age, color):
-        self.breed = breed
-        self.size = size
-        self.age = age
-        self.color = color
-
-    def sleep(self):
-        return "awww it fell asleep, isn't it cute?"
-
-    def eat(self):
-        return "niam niam, dog's love food"
-
-    def sit(self):
-        return "dog has sat, now give it a treat!!!"
-
-    def run(self):
-        return "dog ran a bunch and is tired, GIVE IT A TREAT IMMEDIATELY!!!!"
-
-
-dog_1 = Dog("neapolian mastiff", "large", "5 years", "black")
-dog_2 = Dog("maltese", "small", "2 years", "white")
-dog_3 = Dog("chow chow", "medium", "3 years", "brown")
-print(dog_3.sleep())
-print(dog_3.sit())
-print(dog_3.run())
-print(dog_3.eat())
+p1 = Person("elene", "female", 36, 3000)
+print(p1)
+print(p1.expense())
+print(p1.year_amount())
+print(p1.danazogi())
